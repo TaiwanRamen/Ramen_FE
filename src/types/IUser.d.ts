1c29f5c08d63f82b135c7014549af720579447d0
JulianLin549 <sres3416@gmail.com>
2021-05-08 01:34:35 +0800

    login usercontext

--- src/types/IUser.d.ts
+++ src/types/IUser.d.ts
@@ -1,20 +1,37 @@
-import UserRole from '../enums/UserRole';
-export type IUser = {
-    avatar?: string,
+import {UserRole} from '../enums/UserRole';
+export interface IUser {
+    avatar: string ,
     isVerified: boolean,
     userRole: UserRole,
-    hasStore: string[],
-    notifications: string[],
-    followedStore: string[],
-    reviews: string[],
+    hasStore: any,
+    notifications: any,
+    followedStore: any,
+    reviews: any,
     _id: string,
     fbUid: string,
     fbName: string,
-    email?: string,
+    email: string,
     createdAt: string,
     updatedAt: string,
     __v: number
 }
+// import UserRole from '../enums/UserRole';
+// export type IUser = {
+//     avatar?: string,
+//     isVerified: boolean,
+//     userRole: UserRole,
+//     hasStore: string[],
+//     notifications: string[],
+//     followedStore: string[],
+//     reviews: string[],
+//     _id: string,
+//     fbUid: string,
+//     fbName: string,
+//     email?: string,
+//     createdAt: string,
+//     updatedAt: string,
+//     __v: number
+// }
 
 
 
